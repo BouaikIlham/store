a4287aff11fe7bb93313eaf14cd949d09d12e5cd
ilham bouaik <ibouaik@gmail.com>
2023-01-04 13:25:51 +0000

    add badget  number of items in the cart

--- src/component/CartShop.js
+++ src/component/CartShop.js
@@ -9,10 +9,15 @@ const CartShop = () => {
   return (
     <div>
           <div>
-              <svg onClick={() => setCartOpen(!cartOpen)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-28 h-10">
+            <svg onClick={() => setCartOpen(!cartOpen)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-28 h-10">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
             </svg>
           </div>
+          <span className="absolute object-right-top -mr-6 -my-8 ml-16">
+            <div className="inline-flex items-center px-1.5 py-0.5 border-2 border-white rounded-full text-xs font-semibold leading-4 bg-red-500 text-white">
+                {store.count}
+            </div>
+           </span>
     
           <div className={`relative z-10 ${cartOpen ? "" : "hidden"}`} aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
               <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
--- src/store/store.js
+++ src/store/store.js
@@ -7,6 +7,7 @@ let store = (set) => ({
     cart: [],
     Product: [],
     total : 0,
+    count : 0,
     fetchProducts: async () => {
         const res = await fetch("https://fakestoreapi.com/products")
         .then((res) => res.json())
@@ -15,7 +16,7 @@ let store = (set) => ({
         set({ products: res, isLoading: false });
     },
     addToCart: (product) => {
-        const { cart } = useStore.getState();
+        const { cart , count } = useStore.getState();
         const isFound = cart.some((p) => {
             if (p.id === product.id) {
                 return true;
@@ -25,13 +26,12 @@ let store = (set) => ({
         if (isFound) {
             return alert("Aleardy in cart")
         }
-
         set((state) => ({ cart: [...state.cart, { ...product, number: 1 }]}))
         const { cart: newCart } = useStore.getState();
         let sum = newCart.reduce((acc, value) => acc + value.price, 0)
         sum = sum.toFixed(2)
 
-        set({total: sum})
+        set({total: sum, count: count + 1})
     },
     ProductDetails: (p) => {
         set({ Product: p})
@@ -39,14 +39,14 @@ let store = (set) => ({
     },
 
     clearCart: () => {
-        set({cart: []})
+        set({cart: [], total: 0, count: 0})
     },
     removeProductFromCart: (product) => {
-        const { cart } = useStore.getState()
+        const { cart, count } = useStore.getState()
         const newCart = cart.filter((p) => p.id !== product.id)
         let sum = newCart.reduce((acc, value) => acc + (value.price * value.number), 0)
         sum = sum.toFixed(2)
-        set({cart: newCart, total: sum})
+        set({cart: newCart, total: sum, count: count - 1})
     },
 
     updateTotalCart: () => {
@@ -90,7 +90,6 @@ let store = (set) => ({
         const result = products.filter((p) => {
             return p.category === category
         })
-        console.log(result)
         set({products: result})
         
     }
